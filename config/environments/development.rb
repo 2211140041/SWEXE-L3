4b2ddd9a029d4b275666bd89b960b00605de6385
Shunto Shirasawa <2211140041j@ed.fuk.kindai.ac.jp>
2024-10-02 07:14:26 +0000

    first

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+    config.hosts << "e1ccc1d9970a4b74b1124fea61841802.vfs.cloud9.us-east-1.amazonaws.com"
 end
